05b8e9329b74a486020902b390da7d8ebdca7127
alio-programmer <92017948+alio-programmer@users.noreply.github.com>
2022-03-14 22:47:22 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,8 @@
+#band name generator
+print("Welcome this is a band name generator \n ")
+#taking city name name for refernce 
+city_=input("Enter city in which the band originated \n " )
+#taking pet name to give a cool feel 
+pet_=input("Enter your pet name \n ")
+print("Suggestion for band name is:")
+print(city_ + " "+ pet_)
